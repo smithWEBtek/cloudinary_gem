2f7092c4813d857fc14c32d71e5b6a7933800f7e
daniel cohen <daniel@codeinvain.com>
2017-04-03 16:31:39 +0300

    update code style

--- lib/cloudinary/search.rb
+++ lib/cloudinary/search.rb
@@ -1,39 +1,35 @@
 class Cloudinary::Search
   def initialize
-    @query_hash = {}
-    @query_hash[:sort_by]= []
-    @query_hash[:aggregate]= []
-    @query_hash[:includes]= []
-  end
-
-  ## implicit execute and delegate method to the result hash
-  def method_missing(method_name, *arguments, &block)
-    result = self.execute
-    return result.send(method_name,*arguments)
+    @query_hash = {
+      :sort_by    => [],
+      :aggregate  => [],
+      :includes   => []
+    }
   end
 
   ## implicitly generate an instance delegate the method
-  def self.method_missing(method_name, *arguments, &block)
-    instance = self.new
-    instance.send(method_name,*arguments)
+  def self.method_missing(method_name, *arguments)
+    instance = new
+    instance.send(method_name, *arguments)
   end
 
   def expression(value)
-    @query_hash[:expression]= value
+    @query_hash[:expression] = value
     self
   end
 
   def max_results(value)
-    @query_hash[:max_results]= value
-    self
-  end
-  def next_cursor(value)
-    @query_hash[:next_cursor]= value
+    @query_hash[:max_results] = value
     self
   end
 
-  def sort_by(field_name,dir='desc')
-    @query_hash[:sort_by].push({field_name=>dir})
+  def next_cursor(value)
+    @query_hash[:next_cursor] = value
+    self
+  end
+
+  def sort_by(field_name, dir = 'desc')
+    @query_hash[:sort_by].push(field_name => dir)
     self
   end
 
@@ -48,15 +44,11 @@ class Cloudinary::Search
   end
 
   def to_query
-
-    @query_hash.select { |_, value| !value.nil? && !(value.is_a?(Array) && value.empty?)}
+    @query_hash.select { |_, value| !value.nil? && !(value.is_a?(Array) && value.empty?) }
   end
 
   def execute
-    uri = "resources/search"
-    return Cloudinary::Api.call_api(:post,uri,to_query,{content_type: :json})
+    uri = 'resources/search'
+    Cloudinary::Api.call_api(:post, uri, to_query, :content_type=> :json)
   end
 end
-
-
-
